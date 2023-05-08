546c97011e6d95b425a4279268b00d40b299abe0
Calleum Pecqueux <hi@calleum.au>
2023-05-08 20:24:36 +1000

    bitmasks playground pt2

--- bf.c
+++ bf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * Equivalent to `#define DEFAULT 01`, although:
+ *
+ * * this can _only_ be an integer type
+ * * gdb maps the name to the value
+ * * `#define` s will be pre-processed to only show the raw number in gdb
+ */
 enum
 {
     DEFAULT = 01,
@@ -9,22 +16,30 @@ enum
 };
 
 /**
- * Integer to Boolean String Represenation
+ * @brief
+ * Integer to Boolean String Representation
  *
+ * @param s the subject integer to test
+ * @return the string representation of a boolean based on the value of the
+ * subject
  */
-char *itobs(int s) {
+char *
+itobs(int s)
+{
     return s == 1 ? "True" : "False";
 }
 
 /**
- * @brief 
+ * @brief
  * Check that the bits desired by the mask are set.
  *
  * @param f the target int to check the mask against.
  * @param m the bitmask to check.
  * @return 1 if the mask is set, 0 otherwise.
  */
-int chkbit(int f, int m) {
+int
+chkbit(int f, int m)
+{
     return (f & m) == m;
 }
 
@@ -36,8 +51,11 @@ main(void)
     /* Set the DEFAULT and SPECIAL_1 flags ON */
     flags |= (DEFAULT | SPECIAL_1);
 
-    printf("flags = DEFAULT?[%s], SPECIAL_1?[%s], DEF_SPECIAL_1?[%s], SPECIAL_3?[%s]\n",
-           itobs(chkbit(flags, DEFAULT)), itobs(chkbit(flags, SPECIAL_1)), itobs(chkbit(flags, (DEFAULT | SPECIAL_1))), itobs(chkbit(flags, SPECIAL_3)));
+    printf("flags = DEFAULT?[%s], SPECIAL_1?[%s], DEF_SPECIAL_1?[%s], "
+           "SPECIAL_3?[%s]\n",
+           itobs(chkbit(flags, DEFAULT)), itobs(chkbit(flags, SPECIAL_1)),
+           itobs(chkbit(flags, (DEFAULT | SPECIAL_1))),
+           itobs(chkbit(flags, SPECIAL_3)));
 
     return 0;
 }
